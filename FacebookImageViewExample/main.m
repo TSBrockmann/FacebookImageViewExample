571d29ef2c8b12727bc15c7cbe5edd902f6e15d8
Simon Brockmann <simon.brockmann@tobit.com>
2016-02-18 09:35:22 +0100

    Initial Commit

---
+++ FacebookImageViewExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FacebookImageViewExample
+//
+//  Created by Brockmann, Simon on 18.02.16.
+//  Copyright © 2016 TSBrockmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
